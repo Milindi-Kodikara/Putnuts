356b7e024951451174305c7594af75c5a7782913
Milindi_Kodikara <milindikodikara@gmail.com>
2024-08-29 19:34:46 +1000

    Add TODOs for pre-processing, add term freq for un-processed data

--- helper.py
+++ helper.py
@@ -11,16 +11,25 @@ def process(text, tokeniser, stop_words):
 
         @returns: list of (valid) tokens in text
     """
+    # conversion to lowercase
     text = text.lower()
 
+    # tokenizer
     tokens = tokeniser.tokenize(text)
+
+    # strip whitespace
     tokens_stripped = [tok.strip() for tok in tokens]
 
-    # pattern for digits
-    # the list comprehension in return statement essentially remove all strings of digits or fractions, e.g., 6.15
+    # TODO: Remove abbreviations and replace acronyms with natural language words
+    # TODO: Remove emojis
+    # TODO: Remove user name tags and mentions
+
+    # regex pattern for digits
+    # Note: the list comprehension in return statement essentially remove all strings of digits or fractions, e.g., 6.15
     regex_digit = re.compile("^\d+\s|\s\d+\s|\s\d+$")
-    # regex pattern for http
+    # regex pattern to remove http
     regex_http = re.compile("^http")
 
+    # remove stop words, http, remove digits (with decimals and fractions)
     return [tok for tok in tokens_stripped if
             tok not in stop_words and regex_digit.match(tok) == None and regex_http.match(tok) == None]
