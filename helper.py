18aa819cda898ecd380f771c4302c135245458ba
Milindi_Kodikara <milindikodikara@gmail.com>
2024-09-01 00:13:29 +1000

    Add topic modelling, word cloud

--- helper.py
+++ helper.py
@@ -1,11 +1,14 @@
 # File containing helper functions for sentiment analysis
 # @author Milindi Kodikara, RMIT University, 2024
-
+import math
 from collections import Counter
 import re
 
+import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 from nltk.sentiment import SentimentIntensityAnalyzer
+from wordcloud import WordCloud
 
 import charts
 from utils import *
@@ -31,6 +34,7 @@ def process(text, tokeniser, stop_words):
     # TODO: Remove abbreviations and replace acronyms with natural language words
     # TODO: Remove emojis
     # TODO: Remove user name tags and mentions
+    # TODO: Maybe stemmer
 
     # regex pattern for digits
     # Note: the list comprehension in return statement essentially remove all strings of digits or fractions, e.g., 6.15
@@ -136,3 +140,49 @@ def sentiment_analysis(method, omitb_df, b_print):
             print_coloured_tokens(method, token_list, sentiment, set_pos_words, set_neg_words)
 
     return sentiment_list
+
+
+# TODO: Move to visualization.py file
+def display_topics(model, feature_names, num_top_words):
+    """
+    Prints out the most associated words for each feature.
+
+    @param model: lda model
+    @param feature_names: list of strings, representing the list of features/words.
+    @param num_top_words: number of words to print per topic.
+    """
+
+    # print out the topic distributions
+    for topic_id, topic_distribution_list in enumerate(model.components_):
+        print("Topic %d:" % (topic_id))
+        print(" ".join([feature_names[i] for i in topic_distribution_list.argsort()[:-num_top_words - 1:-1]]))
+
+
+def display_word_cloud(model, feature_names):
+    """
+    Displays the word cloud of the topic distributions, stored in model.
+
+    @param model: lda model.
+    @param feature_names: list of strings, representing the list of features/words.
+    """
+
+    # this normalises each row/topic to sum to one
+    # use this normalised_components to display your wordclouds
+    normalised_components = model.components_ / model.components_.sum(axis=1)[:, np.newaxis]
+
+    topic_num = len(model.components_)
+    # number of wordclouds for each row
+    plot_col_num = 3
+    # number of wordclouds for each column
+    plot_row_num = int(math.ceil(topic_num / plot_col_num))
+
+    for topicId, lTopicDist in enumerate(normalised_components):
+        l_word_prob = {feature_names[i]: wordProb for i, wordProb in enumerate(lTopicDist)}
+        wordcloud = WordCloud(background_color='black')
+        wordcloud.fit_words(frequencies=l_word_prob)
+        plt.subplot(plot_row_num, plot_col_num, topicId + 1)
+        plt.title('Topic %d:' % (topicId + 1))
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis("off")
+
+    plt.show(block=True)
